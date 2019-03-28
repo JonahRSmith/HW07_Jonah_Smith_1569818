5d1d1a277a1f4412de68c0455a974c1aca5bc892
JonahRSmith <49011809+JonahRSmith@users.noreply.github.com>
2019-03-27 23:28:23 -0500

    Solution Routine "Working"
    The solution routine now runs through (still on part 2.a.), but the solution is unexpected, veering off to one side before settling down, whereas the expected solution would be to flatten out in the middle (using k=1)

--- HW7_Master_JRS.m
+++ HW7_Master_JRS.m
@@ -22,4 +22,58 @@ D = 0.1; %Diffusivity
 g0 = 0;
 gL = 0;
 k = 1;
-f = @(x) sin(k*x);
+
+Nodes = 100; %Control number of nodes across the domain 0 <= x <= L
+dx = L/(Nodes-1);
+x = dx:dx:L-dx;
+TimeSteps = 1000; %Set the number of time steps your solution routine takes
+dt = (TIMEND - TIMEO)/TimeSteps;
+
+u = sin(k*x); %Define initial condition for u; u holds the current sol'n for u
+uprev = u; %uprev holds the solution for u at the previous timestep
+
+
+TIMEN = TIMEO; %Current time
+CONST = D*dt/2/dx/dx; %Constant appearing many times through code
+DNODES = length(x); %Number of diffusion nodes (i.e. not boundary nodes)
+figure(1);
+ct=0;
+while (TIMEN < TIMEND)
+    ct = ct+1;
+    %Forward through the tri-diagonal!
+    alpha = zeros(DNODES);
+    g = zeros(DNODES);
+    a = (1 + 2*CONST); %constant in all rows here
+    b = -CONST; %Constant, except where undefined in row(1)
+    c = -CONST; %Constant, except where undefined in row(DNODES)
+    alpha(1) = 1 + 2*CONST;
+    g(1) = uprev(1) + CONST*(2*g0 - 2*uprev(1) + uprev(2));
+    for j=2:DNODES-1
+        alpha(j) = a - (b*c)/alpha(j-1);
+        g(j) = (uprev(j) + CONST*(uprev(j-1) - 2*uprev(j) + uprev(j+1))) - (b*g(j-1)/alpha(j-1));
+    end
+    alpha(DNODES) = a - (b*c)/alpha(j-1);
+    g(DNODES) = uprev(DNODES) + CONST*(uprev(DNODES-1) - 2*uprev(DNODES) + 2*gL);
+    u(DNODES) = g(DNODES)/alpha(DNODES);
+    for k=1:DNODES-1
+        u(DNODES-k) = (g(DNODES-k) - c*u(DNODES-k))/alpha(DNODES-k);
+    end
+    TIMEN = TIMEN + dt;
+    uprev=u;
+    if (TIMEN+dt > TIMEND)
+        fprintf('Reducing dt from %g to %g for final iteration.\n',dt,TIMEND-TIMEN);
+        dt = TIMEND-TIMEN; %Make sure we end on TIMEND, and dont overshoot
+    end
+    if (ct == 10)
+        ct = 0;
+        hold on;
+        plot([0,x,L],[g0,u,gL]);
+    end
+end
+fprintf('END OF SCRIPT REACHED.\n');
+
+
+
+
+
+
